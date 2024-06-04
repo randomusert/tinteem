892a65dac5a912f931e88a70f1062e9b20d77d4f
randomusert <eetupajamaki@gmail.com>
2024-06-04 19:23:14 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+//const withMDX = require('@next/mdx')() as an import
+//import * as withMDX from '@next/mdx'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  //pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+//module.exports = withMDX(nextConfig)
