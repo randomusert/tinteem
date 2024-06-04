bc78fbaf27ccee6fcae0e39b0620a96f44e93001
randomusert <eetupajamaki@gmail.com>
2024-06-04 21:10:20 +0300

    try fix to deploy

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <main className="flex min-h-screen flex-col">
       <div className="z-10 max-w-5xl w-full">
-        <h1 className="text-lg underline font-bold">What is tinteeam's goal and who are we behind the scenes?</h1>
+        <h1 className="text-lg underline font-bold">What is tinteeams goal and who are we behind the scenes?</h1>
       </div>
     </main>
   );
