059de8a326a7cfe719554a9c0574170a86505d1b
randomusert <127417963+randomusert@users.noreply.github.com>
2024-06-05 14:47:59 +0000

    downloads page started

---
+++ src/app/downloads/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-sky-200">
+      <div className="z-10 max-w-5xl w-full">
+        <h1 className="text-xl">Downloads page</h1>
+        <h1></h1>
+      </div>
+    </main>
+  );
+}
