1c702d2d4fca4eeb0347b8a21686b49899953a7a
randomusert <127417963+randomusert@users.noreply.github.com>
2024-06-06 13:31:42 +0000

    projects page started

---
+++ src/app/projects/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function Projects() {
+  return (
+    <main className="flex min-h-screen flex-col bg-sky-200">
+      <div className="z-10 max-w-5xl w-full">
+        <h1 className="text-lg underline font-bold">Projects</h1>
+        <h1>Projects that tinteeam is making</h1>
+        <Link href="/">
+            <button className="bg-lime-800">main page</button>
+        </Link>
+      </div>
+    </main>
+  );
+}
